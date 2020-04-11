de6571b870c92b4172cdcea750fda18058a2c8d3
lamine1305 <lamine1305@gmail.com>
2020-04-11 17:35:47 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -34,8 +34,8 @@ public class Main {
        
         System.out.println("fin du programme lamine1305");
 	System.out.println(" - -  - Validation de la part de lamine1306");	
-cfdnvjkkfhvbfgj
-        System.out.println("fin du programme lamine1305 . . .1305");
+
+      stem.out.println("fin du programme lamine1305 . . .1305");
 	
 
     }
